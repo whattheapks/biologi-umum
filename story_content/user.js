747bf916e797c65d7e735eb77b64e1438c20e647
afifmuhammad <afifmuhammad5997@gmail.com>
2024-08-02 08:55:13 +0700

    update v.2

--- story_content/user.js
+++ story_content/user.js
@@ -2,12 +2,6 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "64V9uZpWBSx":
-        Script1();
-        break;
-      case "5jsj312WoSQ":
-        Script2();
-        break;
   }
 }
 
@@ -18,52 +12,4 @@ var object = player.object;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
-window.Script1 = function()
-{
-  const target = object('5jwuetsKdnj');
-const duration = 750;
-const easing = 'ease-out';
-const id = '6m1T1afPdI3';
-const teeterAmount = 4;
-const signs = ['-', '', '-'];
-
-player.addForTriggers(
-id,
-target.animate([
-{ rotate: '0deg' },
-{ rotate: `${signs[0] + teeterAmount}deg` },
-{ rotate: '0deg' },
-{ rotate: `${signs[1] + teeterAmount}deg` },
-{ rotate: '0deg' },
-{ rotate: `${signs[2] + teeterAmount}deg` },
-{ rotate: '0deg' }
-],
-  { fill: 'forwards', duration, easing }
-)
-);
-}
-
-window.Script2 = function()
-{
-  const target = object('5jwuetsKdnj');
-const duration = 750;
-const easing = 'ease-out';
-const id = '64Pxr2nXXoi';
-const shakeAmount = 2;
-player.addForTriggers(
-id,
-target.animate([
-{ translate: '0 0' },
-{ translate: `-${shakeAmount}px 0` },
-{ translate: '0 0' },
-{ translate: `${shakeAmount}px 0` },
-{ translate: '0 0' },
-{ translate: `-${shakeAmount}px 0` },
-{ translate: '0 0' }
-],
-  { fill: 'forwards', duration, easing }
-)
-);
-}
-
 };
